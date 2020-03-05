3f8ed70bc652576d70cd38a572a52739a01fe039
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-03-05 09:56:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,11 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+ cheese_types = ["cheddar", "gouda", "camembert"]
+ 
+ 
+ 
+ 
+ 
+ 
 end
